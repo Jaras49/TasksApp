0f0a059d108bd8174516ec4de77955bab0fdbebb
Jaras49 <jaras48@gmail.com>
2018-05-22 15:43:29 +0200

    Added message for TaskNotFoundException

--- src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
+++ src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
@@ -1,4 +1,8 @@
 package com.crud.tasks.controller;
 
 public class TaskNotFoundException extends Exception {
+
+    public TaskNotFoundException() {
+        super("No task with specified id");
+    }
 }
