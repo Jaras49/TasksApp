6d103fae9f3b850a217606c3c9a4bdde7411c18b
Jaras49 <jaras48@gmail.com>
2018-06-23 17:33:01 +0200

    fix

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -35,7 +35,7 @@ public class SimpleEmailService {
         simpleMailMessage.setSubject(mail.getSubject());
         simpleMailMessage.setText(mail.getMessage());
         if (!mail.getToCC().isEmpty()) {
-            simpleMailMessage.setCc(mail.getToCC().toArray(new String[]{}));
+            simpleMailMessage.setCc(mail.getToCC().toArray(new String[0]));
         }
 
         return simpleMailMessage;
