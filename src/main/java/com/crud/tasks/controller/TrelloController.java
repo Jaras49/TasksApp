defc84e355a24dce12795fc67ad59b80b325598f
Jaras49 <jaras48@gmail.com>
2018-07-08 13:30:18 +0200

    TrelloFacade implementation

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,7 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.trello.card.CreatedTrelloCardDto;
 import com.crud.tasks.domain.trello.board.TrelloBoardDto;
 import com.crud.tasks.domain.trello.card.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.facade.TrelloFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +17,17 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class TrelloController {
 
     @Autowired
-    private TrelloService trelloService;
+    private TrelloFacade trelloFacade;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
 
-        return trelloService.fetchTrelloBoards();
+        return trelloFacade.fetchTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard", consumes = APPLICATION_JSON_VALUE)
     public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
 
-        return trelloService.createTrelloCard(trelloCardDto);
+        return trelloFacade.createCard(trelloCardDto);
     }
 }
---
+++ src/main/java/com/crud/tasks/mapper/TrelloMapper.java
@@ -0,0 +1,52 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.trello.board.TrelloBoard;
+import com.crud.tasks.domain.trello.board.TrelloBoardDto;
+import com.crud.tasks.domain.trello.board.TrelloList;
+import com.crud.tasks.domain.trello.board.TrelloListDto;
+import com.crud.tasks.domain.trello.card.TrelloCard;
+import com.crud.tasks.domain.trello.card.TrelloCardDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class TrelloMapper {
+
+    public List<TrelloBoard> mapToBoards(final List<TrelloBoardDto> trelloBoardDtos) {
+        return trelloBoardDtos.stream()
+                .map(trelloBoardDto ->
+                        new TrelloBoard(trelloBoardDto.getId(), trelloBoardDto.getName(), mapToList(trelloBoardDto.getLists())))
+                .collect(toList());
+
+    }
+
+    public List<TrelloBoardDto> mapToBoardsDto(final List<TrelloBoard> trelloBoards) {
+        return trelloBoards.stream()
+                .map(trelloBoard ->
+                        new TrelloBoardDto(trelloBoard.getId(), trelloBoard.getName(), mapToListDto(trelloBoard.getLists())))
+                .collect(toList());
+    }
+
+    public List<TrelloList> mapToList(final List<TrelloListDto> trelloListDtos) {
+        return trelloListDtos.stream()
+                .map(trelloListDto -> new TrelloList(trelloListDto.getId(), trelloListDto.getName(), trelloListDto.isClosed()))
+                .collect(toList());
+    }
+
+    public List<TrelloListDto> mapToListDto(final List<TrelloList> trelloLists) {
+        return trelloLists.stream()
+                .map(trelloList -> new TrelloListDto(trelloList.getId(), trelloList.getName(), trelloList.isClosed()))
+                .collect(toList());
+    }
+
+    public TrelloCardDto mapToCardDto(final TrelloCard trelloCard) {
+        return new TrelloCardDto(trelloCard.getName(), trelloCard.getDescription(), trelloCard.getPos(), trelloCard.getPos());
+    }
+
+    public TrelloCard mapToCard(final TrelloCardDto trelloCardDto) {
+        return new TrelloCard(trelloCardDto.getName(), trelloCardDto.getDescription(), trelloCardDto.getPos(), trelloCardDto.getListId());
+    }
+}
