ba3904faa83635ab98962007cd4f1b6a3afe7af5
Jaras49 <jaras48@gmail.com>
2018-07-23 16:36:25 +0200

    fix

--- src/main/java/com/crud/tasks/config/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/config/StaticWebPageController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
-
 @Controller
 public class StaticWebPageController {
 
     @RequestMapping("/")
     public String index(ModelMap model) {
 
-        model.addAttribute("variable", "thymeleaf variable is here");
+        model.addAttribute("variable", "My Thymeleaf variable");
         model.addAttribute("one", 1);
         model.addAttribute("two", 2);
 
